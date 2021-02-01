baa1073eff70d36d5775016e8b8eedac15ded894
lethal8723 <kei.hanafusa@gmail.com>
2021-02-01 12:39:25 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Wife
+# Wife.jl
+A julia package to make my wife's work more efficient :heart:
+
+## Installation
+```julia
+julia>]
+(pkg)>add https://github.com/lethal8723/Wife.jl
+```
+
+## How to use
+### Read Abaqus output file
+```julia
+julia>using Wife
+julia>df = DatToDataFrame("foo.dat")
+```
+
--- src/Wife.jl
+++ src/Wife.jl
@@ -1,5 +1,37 @@
 module Wife
 
 # Write your package code here.
+using DataFrames, DelimitedFiles 
+
+export DatToDataFrame
+
+
+"""
+    skipline(f::AbstractString)
+
+Read Abaqus output file(with .dat extension) and 
+return the number of header rows.
+"""
+function skipline(f)
+    @assert occursin(r"\.dat$",f)
+    skiplinenum=1
+    for line in readlines(f)
+        occursin(r"^-{10,}$",line) && break
+        skiplinenum +=1
+    end
+    return skiplinenum
+end
+
+"""
+    DatToDataFrame(f::AbstractString)
+
+Read Abaqus output file(with .dat extension) and 
+transform it into a DataFrame.
+"""
+function DatToDataFrame(f)
+    skipstart = skipline(f)
+    colnames = split(readlines(f)[skipstart-2])
+    DataFrame(readdlm(f,skipstart=skipstart),colnames)
+end
 
 end
